fca0f991beb30482e7132f4614e6b4a46e7331be
Masterain <i@irain.in>
2024-03-12 16:37:26 -0700

    Update script

--- tinify_utils.py
+++ tinify_utils.py
@@ -4,14 +4,15 @@ import tinify
 def process_img(local_img_path: str, output_folder: str, api_key: str) -> bool:
     tinify.key = api_key
     if "/" in local_img_path:
-        file_name_no_extension = local_img_path.split("/")[-1].split(".")[0]
+        file_name = local_img_path.split("/")[-1]
     elif "\\" in local_img_path:
-        file_name_no_extension = local_img_path.split("\\")[-1].split(".")[0]
+        file_name = local_img_path.split("\\")[-1]
     else:
         raise ValueError("Invalid file path")
     source = tinify.from_file(local_img_path)
     #converted = source.convert(type="image/png")
     #extension = converted.result().extension
-    output_file_path = output_folder + "/" + file_name_no_extension + ".png"
+    output_file_path = output_folder + "/" + file_name
     source.to_file(output_file_path)
+    print(f"Compressed {local_img_path} to {output_file_path} with API key {api_key}")
     return True
