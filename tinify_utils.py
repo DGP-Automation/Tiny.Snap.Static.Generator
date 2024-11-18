24c4c3641d58701fc5560948138be34ccc9ced42
Masterain <i@irain.in>
2024-11-18 03:14:11 -0800

    Update tinify_utils.py

--- tinify_utils.py
+++ tinify_utils.py
@@ -1,6 +1,9 @@
 import threading
-
 import tinify
+import os
+
+from tinify import AccountError
+
 from config.config import TINY_PNG_API_LIST
 
 ALLOWED_MIME = [
@@ -12,24 +15,39 @@ ALLOWED_MIME = [
 lock = threading.Lock()
 api_index = 0
 
+import certifi
+import sys
+
+if hasattr(sys, '_MEIPASS'):
+    certifi_path = os.path.join(sys._MEIPASS, 'certifi', 'cacert.pem')
+else:
+    certifi_path = certifi.where()
+print(f"Using certifi path: {certifi_path}")
+
 
 def process_img(local_img_path: str, output_folder: str, convert_to: str = None) -> bool:
+    # Check for allowed MIME type
     if convert_to not in ALLOWED_MIME and convert_to is not None:
-        print(f"{convert_to} is not an allowed mime type")
+        print(f"{convert_to} is not an allowed MIME type")
         return False
-    origin_type = local_img_path.split(".")[-1]
 
-    if "/" in local_img_path:
-        file_name = local_img_path.split("/")[-1]
-    elif "\\" in local_img_path:
-        file_name = local_img_path.split("\\")[-1]
-    else:
-        raise ValueError("Invalid file path")
+    # Extract file name and extension safely
+    file_name = os.path.basename(local_img_path)
+    origin_type = os.path.splitext(file_name)[-1].lstrip(".")
+
+    if not origin_type:
+        raise ValueError("Unable to determine the file type")
+
+    # Create full output path with updated extension
+    converted_extension = convert_to if convert_to else origin_type
+    output_file_name = file_name.replace(origin_type, converted_extension)
+    output_file_path = os.path.join(output_folder, output_file_name)
+
+    # Process image with Tinify
     source = tinify.from_file(local_img_path)
-    if convert_to:
-        if origin_type not in convert_to:
-            source = source.convert(type=f"image/{convert_to}")
-    output_file_path = output_folder + "/" + file_name.replace(origin_type, convert_to if convert_to else origin_type)
+    source.client.Client.__init__ = certifi_path
+    if convert_to and origin_type != convert_to:
+        source = source.convert(type=f"image/{convert_to}")
     source.to_file(output_file_path)
     print(f"Compressed {local_img_path} to {output_file_path} with API key {tinify.key}")
     return True
@@ -40,7 +58,7 @@ def change_api_key(e):
         return
     with lock:
         global api_index
-        print(f"API key {tinify.key} has reached its limit -> {e}")
+        print(f"API key {tinify.key} has reached its limit -> {str(e)}")
         api_index += 1
         if api_index >= len(TINY_PNG_API_LIST):
             raise RuntimeError("All API keys have reached their limit")
